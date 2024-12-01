fc4771f7337848757ea2968d7fd32a5999cc97f2
Ajinkya Nawarkar <anawarkar@ConverseMac.local>
2024-11-30 19:52:56 -0500

    add param for accepting requests

--- client/curl_request.sh
+++ client/curl_request.sh
@@ -1,5 +1,11 @@
 #!/bin/sh  
 
+if [ -z "$1" ]; then
+    NUM_REQUESTS=100 
+else
+    NUM_REQUESTS=$1  
+fi
+
 LOG_FILE="./logs/quic_log.txt"
 > "$LOG_FILE"
 
@@ -7,7 +13,7 @@ URLS="https://localhost/video/milkyway.mp4 https://localhost/web/"
 
 for url in $URLS; do
     echo "Testing $url" >> "$LOG_FILE"
-    for i in $(seq 1 10); do
+    for i in $(seq 1 "$NUM_REQUESTS"); do
         echo "Request $i to $url:" >> "$LOG_FILE"
         curl --insecure --http3 -I -w "\nTime to First Byte: %{time_starttransfer}s\nConnection Time: %{time_connect}s\nTotal Time: %{time_total}s\n" -o /dev/null "$url" >> "$LOG_FILE" 2>&1
         echo "\n-------------------------" >> "$LOG_FILE"
